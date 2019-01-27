4cdd92f8dc8a6efc4e821d7d2bcd1af7deece223
prithaupadhyay <prithaupadhyay@gmail.com>
2019-01-28 00:58:17 +0530

    Initialized the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'news-app';
+}
