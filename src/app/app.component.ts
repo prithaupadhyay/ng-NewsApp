ffdad91ef724470f19ec5cac0ff20dae0b8d51e9
prithaupadhyay <prithaupadhyay@gmail.com>
2019-01-28 17:03:13 +0530

    SetSource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 import { NewsApiService } from './news-api.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'news-app';
+
+  mArticles: Array<any>;
+  mSources: Array<any>;
+  constructor(private newsapi: NewsApiService) {
+    console.log('app component constructor called');
+  }
+
+  ngOnInit () {
+    this.newsapi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    this.newsapi.initSources().subscribe(data => this.mSources = data['sources']);
+  }
+
+
+  searchArticles(source) {
+    console.log('selected source is: ' + source);
+    this.newsapi.getArticlesByID(source).subscribe(data => this.mArticles = data['articles']);
+  }
 }
